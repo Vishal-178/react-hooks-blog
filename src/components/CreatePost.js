aaeded24d39492b008d45c8dc5d806eacf8b8c7f
vishal singh <vishalkumarmps@gmail.com>
2022-05-03 15:16:03 +0530

    create post form

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,4 +1,41 @@
+import { useState } from 'react';
+
 function CreatePost() {
-  return <div className="">CreatePost</div>;
+  const [title, setTitle] = useState();
+  const [subtitle, setSubtitle] = useState();
+  const [content, setContent] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('title', title);
+    console.log('subtitle', subtitle);
+    console.log('content', content);
+  }
+  return (
+    <div className="create-post">
+      <h1>Create Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Title</label>
+          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+        <div className="form-field">
+          <label>Subtitle</label>
+          <input
+            value={subtitle}
+            onChange={(e) => setSubtitle(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label>Content</label>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button className="create-post-btn">Create Post</button>
+      </form>
+    </div>
+  );
 }
 export default CreatePost;
