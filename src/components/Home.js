bb4660416b00a647bd099aec3bed4e1de1cc2b31
vishal singh <vishalkumarmps@gmail.com>
2022-05-03 16:38:20 +0530

    complete worked on postdetail

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,37 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { fire } from '../firebase';
 function Home() {
-  return <div className="">Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fire
+      .collection('post')
+      .get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        console.log('posts--', posts);
+        setPosts(posts);
+      });
+  }, []);
+  return (
+    <div className="home">
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Vishal Singh</div>
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default Home;
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,4 +1,24 @@
+import { useEffect, useState } from 'react';
+import { fire } from '../firebase';
+import { useParams } from 'react-router-dom';
 function PostDetail() {
-  return <div className="">PostDetail</div>;
+  const [post, setPost] = useState({});
+  const { postId } = useParams();
+  useEffect(() => {
+    fire
+      .collection('post')
+      .doc(postId)
+      .get()
+      .then((snapshot) => {
+        console.log(snapshot.data());
+        setPost(snapshot.data());
+      });
+  }, []);
+  return (
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
 }
 export default PostDetail;
